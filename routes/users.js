fa05a6f6a37c053e092860eee236c85b139f7c95
RRO (RSL-Md. Motiur Rahman) <RRO@rahimafrooz.com>
2023-03-20 14:09:38 +0600

    update code

--- routes/users.js
+++ routes/users.js
@@ -5,25 +5,44 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", async function (req, res, next) {
-    const result = await getDb.collection("customer").find().toArray();
-    res.json({ success: true, users: result });
+  const result = await getDb.collection("customer").find().toArray();
+  res.json({ success: true, users: result });
 });
 
-/* GET users listing. */
+/* GET users listing with ID */
+router.get("/:id", async function (req, res, next) {
+  const id = req.params.id;
+  const result = await getDb
+    .collection("customer")
+    .findOne({ _id: new ObjectId(id) });
+  return res.json(result);
+});
+
+/* POST users listing. */
 router.post("/", async function (req, res, next) {
-    const data = req.body;
-    const result = await getDb.collection("customer").insertOne(data);
-    res.send(result);
+  const data = req.body;
+  const result = await getDb.collection("customer").insertOne(data);
+  res.send(result);
 });
 
-/* GET users listing. */
+/* Update users listing. */
+router.put("/", async function (req, res, next) {
+  const id = req.query.id;
+  const data = req.body;
+  const result = await getDb
+    .collection("customer")
+    .updateOne({ _id: new ObjectId(id) }, { $set: data });
+  res.send(result);
+});
+
+/* Delete users listing. */
 router.delete("/", async function (req, res, next) {
-    const id = req.query.id;
-    console.log(id);
-    const result = await getDb
-        .collection("customer")
-        .deleteOne({ _id: new ObjectId(id) });
-    return res.send({ success: true });
+  const id = req.query.id;
+  console.log(id);
+  const result = await getDb
+    .collection("customer")
+    .deleteOne({ _id: new ObjectId(id) });
+  return res.send({ success: true });
 });
 
 module.exports = router;
